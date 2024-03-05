abc3ffcf756d3d5363274f7272d08fbae99775bb
Abdul12majid <yisaabdulmajid@gmail.com>
2024-03-05 07:40:37 +0100

    try/exept

--- get_header.py
+++ get_header.py
@@ -1,22 +1,32 @@
 import pandas as pd
 from openpyxl import load_workbook
+import time
 
 # Data file paths (replace with actual names)
-name_of_file = input("Provide name of file to get header: ")
+try:
+
+	name_of_file = input("Provide name of file to get header: ")
 
 
-
-file_path = f"file/{name_of_file}.xlsx"
-
-
-# Read data from both Excel files, specifying the header rows
-file_data = pd.read_excel(file_path, header=1)  # Read from the first row (header)
+	file_path = f"file/{name_of_file}.xlsx"
 
 
+	# Read data from both Excel files, specifying the header rows
+	file_data = pd.read_excel(file_path, header=0, dtype={'Opp ID': str})  # Read from the first row (header)
 
 
+	x = file_data.columns.tolist()
 
+	if 'Unnamed: 0' in x and 'Unnamed: 1' in x:
+		print('Invalid column name, checking next header')
 
-print(file_data.columns.tolist())
+		time.sleep(1)
 
+		file_data = pd.read_excel(file_path, header=1, dtype={'Opp ID': str})  # Read from the first row (header)
 
+		print(file_data.columns.tolist())
+
+except FileNotFoundError:
+	time.sleep(2)
+
+	print("Name not found, Kindly check your file folder")
--- main.py
+++ main.py
@@ -3,65 +3,70 @@ from openpyxl import load_workbook
 import time
 
 # Data file paths (replace with actual names of files)
-deal_file = input("Provide name of deal file: ")
-team_file = input("Provide name of your team file: ")
+try:
+    deal_file = input("Provide name of deal file: ")
+    team_file = input("Provide name of your team file: ")
 
-sales_data_path = f"file/{deal_file}.xlsx"
-team_data_path = f"file/{team_file}.xlsx"
+    sales_data_path = f"file/{deal_file}.xlsx"
+    team_data_path = f"file/{team_file}.xlsx"
 
-# Read data from both Excel files, specifying the header rows
-sales_data = pd.read_excel(sales_data_path, header=1, dtype={'Opp ID': str})
-team_data = pd.read_excel(team_data_path, header=1, dtype={'Opportunity ID': str})
-
-
-# Ensure unique index for merging
-sales_data = sales_data.reset_index(drop=True)  # Create a new, unique integer index
-
-# Merge data based on the correct joining column
-merged_data = sales_data.merge(team_data, left_on="Opp ID", right_on="Opportunity ID", how="left")
-
-# Add "Supported" column, adjusting for potential column name differences
-supported_column_name = "Opp ID" if "Opp ID" in merged_data.columns else "Opportunity ID"
-merged_data["CST Support"] = merged_data["Opp ID"].isin(team_data["Opportunity ID"]).map({True: "Supported", False: "Not supported"})
-
-# Create the final report without setting an index
-writer = pd.ExcelWriter("weekly_report.xlsx")
-merged_data.to_excel(writer, sheet_name="Report", index=False)
-writer._save()  # Prefer using 'save' for clarity i.e writer.save()
-
-print("Report generation complete!")
-
-time.sleep(2)
-
-# Renaming columns
-
-print("Renaming columns...")
-
-time.sleep(2)
+    # Read data from both Excel files, specifying the header rows
+    sales_data = pd.read_excel(sales_data_path, header=1, dtype={'Opp ID': str})
+    team_data = pd.read_excel(team_data_path, header=1, dtype={'Opportunity ID': str})
 
 
-new_file = "weekly_report.xlsx"
+    # Ensure unique index for merging
+    sales_data = sales_data.reset_index(drop=True)  # Create a new, unique integer index
 
-read_file = pd.read_excel(new_file, header=0)
+    # Merge data based on the correct joining column
+    merged_data = sales_data.merge(team_data, left_on="Opp ID", right_on="Opportunity ID", how="left")
 
-read_file = read_file.rename(columns={
-    "Unnamed: 21": "ADRM + Upside",
-    "Unnamed: 22": "ISBN (L2)",
-    "Unnamed: 23": "BTP (L2)",
-    "Unnamed: 24": "CX (L2)",
-    "Unnamed: 25": "ERP (L2)",
-    "Unnamed: 26": "HXM (L2)",
-    "Unnamed: 28": "SAP Signavio (L2)",
-    "Unnamed: 29": "S4 Public cloud (L3)",
-    "Unnamed: 30": "S4 Private cloud (L3)",
-    })
+    # Add "Supported" column, adjusting for potential column name differences
+    supported_column_name = "Opp ID" if "Opp ID" in merged_data.columns else "Opportunity ID"
+    merged_data["CST Support"] = merged_data["Opp ID"].isin(team_data["Opportunity ID"]).map({True: "Supported", False: "Not supported"})
 
-writer = pd.ExcelWriter(new_file, engine="openpyxl")
-read_file.to_excel(writer, sheet_name="sheet2", index=False)
-writer._save()
+    # Create the final report without setting an index
+    writer = pd.ExcelWriter("weekly_report.xlsx")
+    merged_data.to_excel(writer, sheet_name="Report", index=False)
+    writer._save()  # Prefer using 'save' for clarity i.e writer.save()
 
-print("Modifications complete")
+    print("Report generation complete!")
 
-time.sleep(2)
+    time.sleep(2)
 
-print("Operation completed")
+    # Renaming columns
+
+    print("Renaming columns...")
+
+    time.sleep(2)
+
+
+    new_file = "weekly_report.xlsx"
+
+    read_file = pd.read_excel(new_file, header=0)
+
+    read_file = read_file.rename(columns={
+        "Unnamed: 21": "ADRM + Upside",
+        "Unnamed: 22": "ISBN (L2)",
+        "Unnamed: 23": "BTP (L2)",
+        "Unnamed: 24": "CX (L2)",
+        "Unnamed: 25": "ERP (L2)",
+        "Unnamed: 26": "HXM (L2)",
+        "Unnamed: 28": "SAP Signavio (L2)",
+        "Unnamed: 29": "S4 Public cloud (L3)",
+        "Unnamed: 30": "S4 Private cloud (L3)",
+        })
+
+    writer = pd.ExcelWriter(new_file, engine="openpyxl")
+    read_file.to_excel(writer, sheet_name="sheet2", index=False)
+    writer._save()
+
+    print("Modifications complete")
+
+    time.sleep(2)
+
+    print("Operation completed")
+
+except FileNotFoundError:
+
+    print("File not Found, check file folder to see name of file")
